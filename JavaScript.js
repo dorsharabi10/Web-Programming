7abe2b80268575bc64aa5de32cfb3293533fb596
dorsharabi10 <156996780+dorsharabi10@users.noreply.github.com>
2024-03-17 01:14:00 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,37 @@
+﻿
+    sum = 0;
+    t = 1;
+    w = 1;
+    function init()
+    {
+        printobj("small", undefined)
+        printobj("medium", undefined)
+    }
+    function disc() {
+        x = document.getElementById("discountTB").value;
+        y = document.getElementById("carType").value;
+        printobj(y, x);
+        }
+    function printobj(Size,count) {
+        let str = "";
+    for (var k in companies[Size]) {
+        for (var j in companies[Size][k]) {
+             if (count != undefined) {
+                 t = 1 - count / 100; }
+                  pri = companies[Size][k][j].price * t;
+                  str +=`<div id="${pri}" onclick="sumi(this.id)">
+                  <h3>${k}</h3>
+                  <h3>${companies[Size][k][j].model}</h3>
+                  <h3>${pri}</h3>
+                  </div>`  }
+               }
+          let namediv = Size + "Cars";
+          document.getElementById(namediv).innerHTML = str;
+       }
+    function sumi(x)
+    {
+        sum += parseFloat(x);
+        if (x == 1000) {sum=0 }
+        document.getElementById("calc").innerHTML = sum;
+    }
+
---
+++ companies.js
@@ -0,0 +1,74 @@
+﻿companies = {
+    small:
+    {
+        hertz: [
+            {
+                model: "mazda 2",
+                price: 100
+            },
+            {
+                model: "fiat 500",
+                price: 80
+            },
+            {
+                model: "kia picanto",
+                price: 50
+            }
+        ],
+        avis: [
+
+            {
+                model: "mazda 2",
+                price: 100
+            },
+
+            {
+                model: "fiat 500",
+                price: 50
+            },
+
+            {
+                model: "suzuki alto",
+                price: 75
+            }
+        ]
+    },
+    medium:
+    {
+        hertz: [
+            {
+                model: "Honda Civic",
+                price: 100
+            },
+            {
+                model: "Kia Nero",
+                price: 80
+            },
+            {
+                model: "Ford Focus",
+                price: 50
+            },
+            {
+                model: "Toyota Corola",
+                price: 50
+            }
+        ],
+        avis: [
+
+            {
+                model: "mazda 2",
+                price: 100
+            },
+
+            {
+                model: "fiat 500",
+                price: 50
+            },
+
+            {
+                model: "suzuki alto",
+                price: 75
+            }
+        ]
+    },
+}
